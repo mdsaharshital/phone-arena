1162b2c11f0318c7a37ab415c6ea3682629d016d
mdsaharshital <mdsaharshitol@gmail.com>
2022-02-28 23:42:07 +0600

    card beautification done

--- js/scriptt.js
+++ js/scriptt.js
@@ -27,13 +27,13 @@
          console.log(mobile);
          const div = document.createElement('div');
          div.innerHTML= `
-            <div class="col" onclick="loadDetails('${mobile.slug}')">
+            <div class="col">
                 <div class="card">
-                    <img src="${mobile.image}" class="card-img-top rounded p-2" alt="..." />
+                    <img src="${mobile.image}" class="mx-auto  rounded p-2" width='224' height='297' alt="..." />
                     <div class="card-body text-center">
                     <h5 class="card-title">Phone Name : ${mobile.phone_name} </h5>
                         <h5 class="card-title">Brand : ${mobile.brand} </h5>
-                        <a href="#" class="btn btn-primary">Details</a>
+                        <a href="#"  onclick="loadDetails('${mobile.slug}')" class="btn btn-primary">Details</a>
                     </div>
                 </div>
             </div>
@@ -58,8 +58,8 @@ const displayDetails= details =>{
 
             <div class="card mb-3 mx-auto" style="max-width: 540px;">
                 <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+                    <div class="col-md-4 d-flex align-items-center py-2">
+                        <img src="${details.image}" class="img-fluid rounded-start mx-auto"  alt="...">
                     </div>
                     <div class="col-md-8">
                         <div class="card-body">

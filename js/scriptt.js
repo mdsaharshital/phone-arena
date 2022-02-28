91d1d6bd1c31f3cee84bc594589e5efddd854fa3
mdsaharshital <mdsaharshitol@gmail.com>
2022-02-28 22:30:46 +0600

    data display done

--- js/scriptt.js
+++ js/scriptt.js
@@ -18,19 +18,21 @@
  }
 
  /* ===========display all phone data ============*/
- const displayData =(mobiles) =>{
+ const displayData =(allmobiles) =>{
+     /* ===========display only 20 phone search data ============*/
+     const mobiles = allmobiles.slice(0,20);
      console.log(mobiles);
      const resultArea = document.getElementById('search-area');
      mobiles.forEach(mobile =>{
          console.log(mobile);
          const div = document.createElement('div');
          div.innerHTML= `
-            <div class="col">
+            <div class="col" onclick="loadDetails('${mobile.slug}')">
                 <div class="card">
                     <img src="${mobile.image}" class="card-img-top rounded p-2" alt="..." />
                     <div class="card-body text-center">
+                    <h5 class="card-title">Phone Name : ${mobile.phone_name} </h5>
                         <h5 class="card-title">Brand : ${mobile.brand} </h5>
-                        <h5 class="card-title">MoDel : ${mobile.phone_name} </h5>
                         <a href="#" class="btn btn-primary">Details</a>
                     </div>
                 </div>
@@ -39,3 +41,21 @@
          resultArea.appendChild(div);
      })
  }
+ /* ================ load Details================ */
+ const loadDetails = async(id) =>{
+     const  url = `https://openapi.programming-hero.com/api/phone/${id}`;
+     const res = await fetch(url);
+     const data = await res.json();
+     displayDetails(data.data)
+    }
+/* ================ Display Details================ */
+const displayDetails= details =>{
+    console.log(details);
+}
+
+ /* <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div> */

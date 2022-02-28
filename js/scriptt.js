e6a229ffa6ee1a1072cda47a0c5714efb475b52e
mdsaharshital <mdsaharshitol@gmail.com>
2022-02-28 22:00:03 +0600

    data load done

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,5 +1,5 @@
 // Phone Search
-// URL Format: https://openapi.programming-hero.com/api/phones?search=${searchText}
+// URL Format: 
 
 // Example: https://openapi.programming-hero.com/api/phones?search=iphone
 
@@ -7,3 +7,35 @@
 // URL Format: https://openapi.programming-hero.com/api/phone/${id}
 
 // Example: https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089
+
+/* =============load all phone data ============ */
+ const loadPhone = async() =>{
+    const searchText = document.getElementById('search-field').value;
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`; 
+    const res = await fetch(url);
+    const data = await res.json();
+    displayData(data.data);
+ }
+
+ /* ===========display all phone data ============*/
+ const displayData =(mobiles) =>{
+     console.log(mobiles);
+     const resultArea = document.getElementById('search-area');
+     mobiles.forEach(mobile =>{
+         console.log(mobile);
+         const div = document.createElement('div');
+         div.innerHTML= `
+            <div class="col">
+                <div class="card">
+                    <img src="${mobile.image}" class="card-img-top rounded p-2" alt="..." />
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Brand : ${mobile.brand} </h5>
+                        <h5 class="card-title">MoDel : ${mobile.phone_name} </h5>
+                        <a href="#" class="btn btn-primary">Details</a>
+                    </div>
+                </div>
+            </div>
+         `;
+         resultArea.appendChild(div);
+     })
+ }
